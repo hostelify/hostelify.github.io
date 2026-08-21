a507a25cab6c3c05f72f6687a89f623ffb65640b
sTucKeD <codewithpavitrr@gmail.com>
2026-08-21 11:09:40 +0530

    Update login.js

--- login.js
+++ login.js
@@ -318,7 +318,7 @@ if (loginForm) {
                         headers: {
                             "Content-Type":
                                 "text/plain;charset=utf-8"
-                        }),
+                        },
 
                         body: JSON.stringify({
 
